d7d70d929f2caaf3d6eec659ecf57e05d094268d
unknown <khaideptrai13907@gmail.com>
2026-03-27 13:11:21 +0700

    sắp hoàn thành

--- src/main.cpp
+++ src/main.cpp
@@ -17,20 +17,88 @@ void Task_1(void *parameter){
     while(1){
         button command = scan();
         unsigned long push_current = millis();
+        
         if(command != button::NONE){
             if(push_current - push_previous > time_push){
                 current_state = ERROR;
                 Serial.println("Now: Error");
-                delay(100);
+                vTaskDelay(100 / portTICK_PERIOD_MS);
             }
             push_previous = push_current;
         }
-        switch(current_state){
-            case IDLE:
-            case RECORD:
-            case PLAYING:
-            case ERROR:
+
+        if(command != button::NONE){    
+            
+            vTaskDelay(20 / portTICK_PERIOD_MS);
+            
+            if(scan() == command) { 
+                
+                switch(current_state){
+                    case IDLE:
+                        if(command == button::OK){
+                            current_state = RECORD;
+                            Serial.println("Now: RECORD");
+                        }
+                        break;
+                        
+                    case RECORD:
+                        if(command == button::OK){
+                            current_state = PLAYING;
+                            Serial.println("Now: PLAYING");
+                        } 
+                        else { 
+                            if(step > 0){
+                                button step_previous = route[step - 1];
+                                if( (step_previous == button::TOP && command == button::BACK) ||
+                                    (step_previous == button::BACK && command == button::TOP) ||
+                                    (step_previous == button::RIGHT && command == button::LEFT) ||
+                                    (step_previous == button::LEFT && command == button::RIGHT) )
+                                {
+                                    current_state = ERROR;
+                                    Serial.println("Now: ERROR");
+                                    vTaskDelay(100 / portTICK_PERIOD_MS); 
+                                    break; // Thoát case
+                                }
+                            }
+                            
+                            if(step < 100){
+                                route[step] = command; 
+                                
+                                // SỬA Ở ĐÂY: In ra màn hình chuẩn C++
+                                Serial.print("Buoc so "); Serial.println(step);
+                                Serial.print("Di "); Serial.println((int)command);
+                                
+                                step++;
+                            }
+                        }
+                        break; 
+                        
+                    case ERROR:
+                        if(command == button::OK){
+                            step = 0;
+                            current_state = IDLE;
+                            Serial.println("Now: IDLE");
+                        }
+                        break;
+                        
+                    case PLAYING:
+                        break;
+                }
+
+                while(scan() != button::NONE) {
+                    vTaskDelay(20 / portTICK_PERIOD_MS);
+                }
+            }
         }
+        if (current_state == PLAYING) {
+
+            step = 0;
+            current_state = IDLE;
+            Serial.println("Now: IDLE");
+            vTaskDelay(100 / portTICK_PERIOD_MS);
+        }
+        
+        vTaskDelay(30 / portTICK_PERIOD_MS);
     }
 }
 
